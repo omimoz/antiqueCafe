380d5be39847404e220e3312c2357f19a953e5dd
omid <o.moazzmi@gmail.com>
2023-06-13 23:49:33 +0330

    fix some issue

--- src/data/Images.js
+++ src/data/Images.js
@@ -1,23 +1,23 @@
 export const images = [
   {
-    url: "./1.jpg",
+    url: "./1b.jpg",
     title: "Antique cafe is coming to Dubai 1",
     id: "Antique cafe: 1",
   },
   {
-    url: "./3.jpg",
+    url: "./3b.jpg",
     id: "Antique cafe: 2",
     title:
       "The taste and smell of the finest and purest coffee ever to be served, will take our guests to the time of surrounding antiques 2",
   },
   {
-    url: "./8.jpg",
+    url: "./8b.jpg",
     id: "Antique cafe: 3",
     title:
       "Antique cafe owns one of the most valuable collections in the middle east 3",
   },
   {
-    url: "./4.jpg",
+    url: "./4b.jpg",
     id: "Antique cafe: 4",
     title:
       "We showcase not just ordinary antiques, but mesmerizing Kings' and high profile characters' belongings 4",
@@ -28,12 +28,12 @@ export const images = [
     title: "It would be an honor to serve time travellers 5",
   },
   {
-    url: "./6.jpg",
+    url: "./6b.jpg",
     id: "Antique cafe: 6",
     title: "Let your soul fly on your visit to Antique Cafe. 6",
   },
   {
-    url: "./7.jpg",
+    url: "./7b.jpg",
     id: "Antique cafe: 7",
     title: "You will have to struggle not to feel mesmerized and hypnotized 7",
   },
